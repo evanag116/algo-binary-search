51199fb4e0e1fc30b0bb4355fd6590d5bd4577bc
Evan Garcia <evanag116@yahoo.com>
2022-06-09 17:08:29 -0500

    completed in py

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,17 @@
-def binary_search():
-    pass
+def binary_search(num, arr):
+    l, r = 0, len(arr)
+
+    while l < r:
+        index = (r + l)//2
+        if num == arr[index]:
+            return index
+        elif num > arr[index]:
+            l = index + 1
+        elif num < arr[index]:
+            r = index - 1
+
+    return -1
+
+
+
+print(binary_search(130, range(100, 200, 3))) # should be 10
